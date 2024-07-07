96e6db1f12417bc73981ec6fd8331ecc57fb9301
ml4g <m.laguna@ebc.edu.mx>
2024-07-07 14:28:05 -0600

    Se agrega al Proyecto

---
+++ Prueba1.java
@@ -0,0 +1,10 @@
+public class DevOpsProyecto {
+
+    public static void main(String[] args) {
+        // Mensaje a imprimir
+        String mensaje = "The world is a vampire";
+
+        // Imprimir el mensaje en la consola
+        System.out.println(mensaje);
+    }
+}
